6360e60f091503e31a09973ae021aa480a78f091
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-04-18 06:53:05 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -443,7 +443,8 @@ getCert() {
 			systemctl start cron
 			systemctl enable cron
 		fi
-		curl -sL https://get.acme.sh | sh -s email=liusiyu@cvte.com
+		autoEmail=$(date +%s%N | md5sum | cut -c 1-32)
+		curl -sL https://get.acme.sh | sh -s email=$autoEmail@gmail.com
 		source ~/.bashrc
 		~/.acme.sh/acme.sh --upgrade --auto-upgrade
 		~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
